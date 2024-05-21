f40fb26860d750938659363ce293d7a3d5635f80
alcozrodrigo <alcoz.rodrigo@gmail.com>
2024-05-21 19:19:18 +0900

    Initial Commit

---
+++ CPM_visual Group8/CPM_visual_Group8App.swift
@@ -0,0 +1,17 @@
+//
+//  CPM_visual_Group8App.swift
+//  CPM_visual Group8
+//
+//  Created by Rodrigo Alcoz Meroño on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CPM_visual_Group8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
